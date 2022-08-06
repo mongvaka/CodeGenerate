6308ffbe7986430e06bcc09e65c384c2577a806f
sanit vakchaiyaphum <sanit.vakch@gmail.com>
2022-08-06 15:02:13 +0700

    delete file

--- export/nest/supplier-code/supplier-code.entity.ts
+++
@@ -1,7 +0,0 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
-import {BasicData} from "../shared/entities/basic-data";
-@Entity('SUPPLIER_CODE')
-export class SupplierCode extends BasicData{
-    @PrimaryGeneratedColumn({type:'bigint', name:'ID'})
-    id :number;
-}
--- src/startGen.ts
+++ src/startGen.ts
@@ -20,25 +20,43 @@ export const startGenerate = async () => {
   // const masterList: CellItemModel[] = await getFileFromExcel([]);
 
   const DOC1: CellItemModel[] =
-    await getFileFromExcelForNest("16 PC_REJECT_AND_CANCEL_REASON_");
-  const DOC2: CellItemModel[] = await getFileFromExcelForNest(
-    "72 PC_FORMAT"
+    await getFileFromExcelForNest("PROPERTY_CONTRACT");
+  const DOC2: CellItemModel[] = 
+  await getFileFromExcelForNest(
+    "PROPERTY_CONTRACT_HAS_SUPPLIER"
   );
-  const DOC3: CellItemModel[] = await getFileFromExcelForNest(
-    "74 PC_DEPARTMENT"
+  const DOC3: CellItemModel[] = 
+  await getFileFromExcelForNest(
+    "SUB_PERIOD"
   );
-  // const DOC4: CellItemModel[] = await getFileFromExcelForNest(
-  //   "TTA_ANALYSIS"
-  // );
-  // const DOC5: CellItemModel[] = await getFileFromExcelForNest(
-  //   "CONTRACT"
-  // );
+  const DOC4: CellItemModel[] = 
+  await getFileFromExcelForNest(
+    "RENTAL_OPTION"
+  );
+  const DOC5: CellItemModel[] = 
+  await getFileFromExcelForNest(
+    "PROPERTY_CONTRACT_HAS_SUB_PE"
+  );
   
-  // const DOC6: CellItemModel[] = await getFileFromExcelForNest("CONTRACT_HAS_FUNDING");
-  // const DOC7: CellItemModel[] =
-  //   await getFileFromExcelForNest("CONTRACT_TYPE");
-  //   const DOC8: CellItemModel[] =
-  //   await getFileFromExcelForNest("STORE");
+  const DOC6: CellItemModel[] = 
+  await getFileFromExcelForNest("PREPAID");
+  const DOC7: CellItemModel[] =
+    await getFileFromExcelForNest("EXPENSE");
+    const DOC8: CellItemModel[] =
+    await getFileFromExcelForNest("DEPOSIT");
+    const DOC9: CellItemModel[] =
+    await getFileFromExcelForNest("LEASE_REQ");
+    const DOC10: CellItemModel[] =
+    await getFileFromExcelForNest("TYPE_OF_CONTRACT");
+    const DOC11: CellItemModel[] =
+    await getFileFromExcelForNest("PROPERTY_CONTRACT_HAS_TYPE");
+    const DOC12: CellItemModel[] =
+    await getFileFromExcelForNest("PROPERTY_CONTRACT_APPROVAL");
+    const DOC13: CellItemModel[] =
+    await getFileFromExcelForNest("PROPERTY_CONTRACT_HISTORY");
+    const DOC14: CellItemModel[] =
+    await getFileFromExcelForNest("PROPERTY_CONTRACT_FILE_UPLOAD");
+
   // const creatAngularService = new CreateAngular(masterList);
   // creatAngularService.createAngularWeb();
   // const createExpress = new CreateExpress(masterList);
@@ -48,18 +66,31 @@ export const startGenerate = async () => {
   const createNest1 = new CreateNest(DOC1);
    const createNest2 = new CreateNest(DOC2);
   const createNest3 = new CreateNest(DOC3);
-  // const createNest4 = new CreateNest(DOC4);
-  // const createNest5 = new CreateNest(DOC5);
-  // const createNest6 = new CreateNest(DOC6);
-  // const createNest7 = new CreateNest(DOC7);
-  // const createNest8 = new CreateNest(DOC8);
+  const createNest4 = new CreateNest(DOC4);
+  const createNest5 = new CreateNest(DOC5);
+  const createNest6 = new CreateNest(DOC6);
+  const createNest7 = new CreateNest(DOC7);
+  const createNest8 = new CreateNest(DOC8);
+  const createNest9 = new CreateNest(DOC9);
+  const createNest10 = new CreateNest(DOC10);
+  const createNest11 = new CreateNest(DOC11);
+  const createNest12 = new CreateNest(DOC12);
+  const createNest13 = new CreateNest(DOC13);
+  const createNest14 = new CreateNest(DOC14);
 
   createNest1.createNestApi();
   createNest2.createNestApi();
   createNest3.createNestApi();
-  // createNest4.createNestApi();
-  // createNest5.createNestApi();
-  // createNest6.createNestApi();
-  // createNest7.createNestApi();
-  // createNest8.createNestApi();
+  createNest4.createNestApi();
+  createNest5.createNestApi();
+  createNest6.createNestApi();
+  createNest7.createNestApi();
+  createNest8.createNestApi();
+  createNest9.createNestApi();
+  createNest10.createNestApi();
+  createNest11.createNestApi();
+  createNest12.createNestApi();
+  createNest13.createNestApi();
+  createNest14.createNestApi();
+
 };
